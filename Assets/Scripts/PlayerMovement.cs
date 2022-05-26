e58e1450a09a56dc8465cb479362bf05f6f36d47
Sean Moon <deccj97@naver.com>
2022-05-26 14:42:49 +0900

    apply 0520 class

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigid;
+    public int speed = 10;
+    float y;
+    float x;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector3 vec = new Vector3(0,0,0);
+
+        y = Input.GetAxis("Vertical");
+        x = Input.GetAxis("Horizontal");
+        vec += new Vector3(x*speed, 0, y*speed);
+
+        rigid.velocity = vec;
+    }
+}
